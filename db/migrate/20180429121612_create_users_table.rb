3029d54a6c2e642a5389f1f26991926bc3751e4b
Jawann <jawann@gmail.com>
2018-04-29 12:18:44 +0000

    automatically backed up by learn

--- db/migrate/20180429121612_create_users_table.rb
+++ db/migrate/20180429121612_create_users_table.rb
@@ -1,7 +1,9 @@
 class CreateUsersTable < ActiveRecord::Migration[5.2]
   def change
-    t.string :name
-    t.string :email
-    t.string :password
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end
   end
 end
